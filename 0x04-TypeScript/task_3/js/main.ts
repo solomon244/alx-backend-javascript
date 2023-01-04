229a041b37c6c4c0301e8d5a269b4eba186fefef
solomon244 <slegesse2444@gmail.com>
2023-01-05 00:32:52 +0300

    update main.ts

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -2,10 +2,10 @@ import { RowID, RowElement } from './interface';
 import * as CRUD from './crud';
 
 const row: RowElement = {
-  firstName: 'yonas',
-  lastName: 'Abebe',
+  firstName: 'Guillaume',
+  lastName: 'Salva',
 };
 const newRowID: RowID = CRUD.insertRow(row);
-const updatedRow: RowElement = {...row, age: 25};
+const updatedRow: RowElement = {...row, age: 23};
 CRUD.updateRow(newRowID, updatedRow);
 CRUD.deleteRow(newRowID);
