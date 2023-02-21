b41d2214d08cbad6b5cf5093a03c6807445e0d2d
solomon244 <slegesse2444@gmail.com>
2023-02-21 21:38:24 +0300

    basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author solomon legesse <https://github.com/solomon244>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
