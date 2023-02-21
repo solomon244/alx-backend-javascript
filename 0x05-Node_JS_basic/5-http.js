d860f70a43ae72a109bb4d1fb65fc049f94ee993
solomon244 <slegesse2444@gmail.com>
2023-02-21 22:12:25 +0300

    Create a more complex HTTP server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -13,12 +13,12 @@ const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
  */
 const countStudents = (dataPath) => new Promise((resolve, reject) => {
   if (!dataPath) {
-    reject(new Error('Cannot load database'));
+    reject(new Error('Cannot load the database'));
   }
   if (dataPath) {
     fs.readFile(dataPath, (err, data) => {
       if (err) {
-        reject(new Error('Cannot load database'));
+        reject(new Error('Cannot load the database'));
       }
       if (data) {
         const reportParts = [];
